00b300f1bb48f63883c10ceb96c8f9a22e74d468
Andrew Allemond <118858494+aallemond@users.noreply.github.com>
2023-04-02 14:08:40 -0500

    started adding function to display current weather on a card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
 var APIKey = "69383b3abe7567a76337fff968d33b7d";
 var city = document.getElementById("searchCity").value;
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + 
+city + 
+"&appid=" + 
+APIKey + 
+"&units=imperial";
 
+const currentData = {};
+const fiveDayData = {};
 
 // var today = moment().format('L');
 var searchHistory = [];
 
-
+// Function to get current weather data from API
 function currentConditions(city) {
     var city = document.getElementById("searchCity").value;
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + 
@@ -19,6 +26,22 @@ function currentConditions(city) {
     fetch(queryURL)
     .then(response => response.json())
     .then(data => console.log(data));
+
+    showCurrent();
+//Function to show a card populated by the data from API for searched city
+function showCurrent(){ 
+
+    const cityDisplayName = document.querySelectorAll(".currentWeatherCityName")
+    const currentDisplayTemp = document.querySelectorAll(".currentWeatherTemp")
+    const currentDisplayConditions = document.querySelectorAll(".currentWeatherCondition")
+    const currentDisplayWind = document.querySelectorAll(".currentWeatherWind")
+
+
+    
+   
 }
+};
+
+
 
 
