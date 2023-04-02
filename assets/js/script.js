c2a3e312ba108475440798cde3b584f7899e2317
Andrew Allemond <118858494+aallemond@users.noreply.github.com>
2023-04-02 13:17:27 -0500

    moved city variable so that the function currentConditions now works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,24 @@
 var APIKey = "69383b3abe7567a76337fff968d33b7d";
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-var today = moment().format('L');
+var city = document.getElementById("searchCity").value;
+
+
+// var today = moment().format('L');
 var searchHistory = [];
 
 
 function currentConditions(city) {
-    
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-    var city = document.getElementById("searchCity").value
+    var city = document.getElementById("searchCity").value;
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + 
+    city + 
+    "&appid=" + 
+    APIKey + 
+    "&units=imperial";
+
+  
 
     fetch(queryURL)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => console.log(data));
 }
 
 
