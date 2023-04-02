64109a3b8f0b77cff56b210540110a8cd3df2ec5
Andrew Allemond <118858494+aallemond@users.noreply.github.com>
2023-04-02 12:26:02 -0500

    finished html and started js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
 var APIKey = "69383b3abe7567a76337fff968d33b7d";
-
-var city;
-
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var today = moment().format('L');
+var searchHistory = [];
+var city = document.getElementById
+
+function currentConditions(city) {
+
+}
 
 fetch(queryURL)
     .then(response => response.json())
